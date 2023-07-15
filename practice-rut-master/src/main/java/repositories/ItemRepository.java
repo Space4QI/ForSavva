8bc9fbb8666872ed84c2cb8f95969384326add36
Space4QI <104172722+Space4QI@users.noreply.github.com>
2023-07-15 22:24:49 +0300

    Add files via upload

---
+++ practice-rut-master/src/main/java/repositories/ItemRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import com.example.practice.models.Game;
+import com.example.practice.models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository <Item, Integer>{
+    public Item findItemByName(String name);
+}
---
+++ practice-rut-master/src/main/java/repositories/UserEntityRepository.java
@@ -0,0 +1,11 @@
+package repositories;
+
+import com.example.practice.models.Game;
+import com.example.practice.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository <UserEntity, Integer>{
+    public UserEntity findUserEntityByNickname(String nickname);
+}
